f9b7a10da88657435a557c7593808f5ad728c3d7
Juan Lee <juanlee@kaist.ac.kr>
2019-04-05 00:47:24 +0900

    Bingle Bangle eyes

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-document.addEventListener("mousemove", (e)=>{
-    
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    var logo = document.getElementById("logo");
+var prevDegLeft = 0;
+var prevDegRight = 0;
 
-    if(e.y < height/2){   
-        logo.src = "./logo_weird.svg"
+document.addEventListener("mousemove", (e)=>{
+    var eyeLeft = document.getElementById("eye-left");
+    var rectLeft = eyeLeft.getBoundingClientRect();
+    var eyePosLeft = {
+        x: rectLeft.x + rectLeft.width / 2,
+        y: rectLeft.y + rectLeft.height / 2,
     }
-    else
-        logo.src = "./logo.svg"
+
+    var eyeRight = document.getElementById("eye-right");
+    var rectRight = eyeRight.getBoundingClientRect();
+    var eyePosRight = {
+        x: rectRight.x + rectRight.width / 2,
+        y: rectRight.y + rectRight.height / 2,
+    }
+
+    // re-position with prevDeg
+    eyePosLeft.x -= 11*Math.cos(prevDegLeft);
+    eyePosLeft.y -= 11*Math.sin(prevDegLeft);
+    eyePosRight.x -= 11*Math.cos(prevDegRight);
+    eyePosRight.y -= 11*Math.sin(prevDegRight);
+
+    var degLeft = Math.atan((e.y - eyePosLeft.y) / (e.x - eyePosLeft.x));
+    if(e.x > eyePosLeft.x){
+        eyeLeft.setAttribute('style', `transform: translate(${11*Math.cos(degLeft)}px, ${11*Math.sin(degLeft)}px)`);
+    }
+    else {
+        eyeLeft.setAttribute('style', `transform: translate(${-11*Math.cos(degLeft)}px, ${-11*Math.sin(degLeft)}px)`);
+    }
+
+    var degRight = Math.atan((e.y - eyePosRight.y) / (e.x - eyePosRight.x));
+    if(e.x > eyePosRight.x){
+        eyeRight.setAttribute('style', `transform: translate(${11*Math.cos(degRight)}px, ${11*Math.sin(degRight)}px)`);
+    }
+    else {
+        eyeRight.setAttribute('style', `transform: translate(${-11*Math.cos(degRight)}px, ${-11*Math.sin(degRight)}px)`);
+    }
+
+    prevDegLeft = degLeft;
+    prevDegRight = degRight;
 })
